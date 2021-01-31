e0c570d72880b1039be3b3ab02916842badbea56
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2021-01-31 21:53:50 +0200

    Tehtävä 3:a kehitetty eteenpäin

--- src/Kaupunkipyorat.js
+++ src/Kaupunkipyorat.js
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Button from '@material-ui/core/Button'
+import Kartta from './Kartta'
 
 const Kaupunkipyorat = () => {
     const [pyorat, setPyorat] = useState([])
+    const [koordinaattiX, setKoordinaattiX] = useState(24.840319000421573)
+    const [koordinaattiY, setKoordinaattiY] = useState(60.16581999969327)
 
     useEffect(() => {
-        axios
-        .get('https://services1.arcgis.com/sswNXkUiRoWtrx0t/arcgis/rest/services/Helsingin_ja_Espoon_kaupunkipy%C3%B6r%C3%A4asemat/FeatureServer/0/query?where=1%3D1&outFields=Kapasiteet,Osoite&outSR=4326&f=json')
-        .then(response => {
-            setPyorat(response.data)
-        })
+        async function fetchdata() {
+            await axios
+                .get('https://services1.arcgis.com/sswNXkUiRoWtrx0t/arcgis/rest/services/Helsingin_ja_Espoon_kaupunkipy%C3%B6r%C3%A4asemat/FeatureServer/0/query?where=1%3D1&outFields=Kapasiteet,Osoite&outSR=4326&f=json')
+                .then(response => {
+                    setPyorat(response.data)
+                })}
+        fetchdata()
     }, [])
+
+    const asetaKoordinaatit = (x, y) => {
+        setKoordinaattiX(x)
+        setKoordinaattiY(y)
+        console.log(koordinaattiX, koordinaattiY)
+    }
 
     return (
         <div>
+            <Kartta y={koordinaattiY} x={koordinaattiX}/>
             {console.log(pyorat)}
             {pyorat.features.map(pyora => {
                 return (
                     <li>
-                        Asema: {pyora.attributes.Osoite}<br/>
+                        Asema
+                        : <Button onClick={() => asetaKoordinaatit(pyora.geometry.x, pyora.geometry.y)}>{pyora.attributes.Osoite}</Button><br/>
                         Kapasiteetti: {pyora.attributes.Kapasiteet}
                     </li>
                 )
